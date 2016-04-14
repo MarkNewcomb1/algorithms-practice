5b73f1392dca22725ae5a388370dc8f7bab9b8f5
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-04-14 15:24:51 -0600

    1, 2, and 3 solved, created index page

--- problem1-solved.js
+++ problem1-solved.js
@@ -2,17 +2,43 @@
 //[3,4,5] -> 3
 //[2, -30] -> -30
 
-function minimum(arr) {
-	var minSoFar = arr[0];
-	for (var i = 1; i < arr.length; i++) {
-		if(arr[i] < minSoFar) {
-			minSoFar = arr[i];
-		}
-	}
-	return minSoFar;
+//function minimum(arr) {
+//	var minSoFar = arr[0];
+//	for (var i = 1; i < arr.length; i++) {
+//		if(arr[i] < minSoFar) {
+//			minSoFar = arr[i];
+//		}
+//	}
+//	return minSoFar;
+//}
+//
+//console.log(minimum([3,4,5]) == 3);
+//console.log(minimum([2, -30]) == -30);
+//console.log(minimum([0,1,2,-3]) == -3);
+//console.log(minimum([0]) == 0);
+
+function maximum(arr) {
+    "use strict";
+    var maxSoFar = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if(arr[i] > maxSoFar) {
+            maxSoFar = arr[i];
+        }
+    }
+    return maxSoFar;
 }
 
-console.log(minimum([3,4,5]) == 3);
-console.log(minimum([2, -30]) == -30);
-console.log(minimum([0,1,2,-3]) == -3);
-console.log(minimum([0]) == 0);
+console.log(maximum([3,4,5]) == 5);
+console.log(maximum([-10, 2]) == 2);
+
+function getTheAverage(arr) {
+   "use strict";
+    var sumSoFar = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        sumSoFar += arr[i];
+        var average = (sumSoFar / arr.length);
+    }
+    return average;
+}
+
+console.log(getTheAverage([2, 4, 6]) == 4);
